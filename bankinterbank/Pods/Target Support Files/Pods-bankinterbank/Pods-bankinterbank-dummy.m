48a8ef9aceb33bb3173c50e1b7c3fab095dd1c36
Miguel Otero <miguel-otero-calderon@hotmail.com>
2022-07-21 19:26:55 -0500

    commit initial

---
+++ bankinterbank/Pods/Target Support Files/Pods-bankinterbank/Pods-bankinterbank-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bankinterbank : NSObject
+@end
+@implementation PodsDummy_Pods_bankinterbank
+@end
---
+++ bankinterbank/bankinterbank/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  bankinterbank
+//
+//  Created by Miguel on 21/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
